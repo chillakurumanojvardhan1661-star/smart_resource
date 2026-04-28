744353542338ea02c68c68c45765a1cb22a465be
The - Sreedhar <sreedhar098@yahoo.com>
2026-04-29 02:47:59 +0530

    fix: wrap app in AuthProvider to resolve context errors and ensure latest changes are visible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,12 @@ import '../app/fonts.css'
 import '../app/globals.css'
 import type { AppProps } from 'next/app'
 
+import { AuthProvider } from '../context/AuthContext'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
